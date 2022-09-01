ac13c774f8fe250127ae8535758d8b324524110e
Timothy Yakhya <yakhya.timothy@gmail.com>
2022-09-01 14:54:17 -0400

    addedPasswordValidation

---
+++ script.js
@@ -0,0 +1,35 @@
+const password = document.querySelector('#password')
+const confirmPassword = document.querySelector('#confirm_password')
+const message = document.querySelector('.message')
+const submit = document.getElementById('#submit')
+
+
+function checkPass(pass,confPass) {
+    pass = document.getElementById("password").value
+    confPass = document.getElementById("confirm_password").value
+
+if (pass !== "" && confPass !== "") {
+    if (pass === confPass) {
+        password.classList.remove('invalid')
+        password.classList.add('valid')
+        confirmPassword.classList.remove('invalid')
+        confirmPassword.classList.add('valid')
+        message.textContent = ""
+    } else {
+        password.classList.remove('valid')
+        password.classList.add('invalid')
+        confirmPassword.classList.remove('valid')
+        confirmPassword.classList.add('invalid')
+        message.textContent = '*Passwords do not match'
+    }
+    } else {
+        password.classList.remove('invalid')
+        confirmPassword.classList.remove('invalid')
+        password.classList.remove('valid')
+        confirmPassword.classList.remove('valid')
+        message.textContent = ""
+    }
+}
+
+password.onchange = checkPass
+confirmPassword.onchange = checkPass
