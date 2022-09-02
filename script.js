c0a0e0fd3a1622e159e8f8f67b8d934a2c59e67c
Timothy Yakhya <yakhya.timothy@gmail.com>
2022-09-02 12:43:34 -0400

    addedPhoneNumberValidation

--- script.js
+++ script.js
@@ -8,27 +8,27 @@ function checkPass(pass,confPass) {
     pass = document.getElementById("password").value
     confPass = document.getElementById("confirm_password").value
 
-if (pass !== "" && confPass !== "") {
-    if (pass === confPass) {
-        password.classList.remove('invalid')
-        password.classList.add('valid')
-        confirmPassword.classList.remove('invalid')
-        confirmPassword.classList.add('valid')
-        message.textContent = ""
+    if (pass !== "" && confPass !== "") {
+        if (pass === confPass) {
+            password.classList.remove('invalid')
+            password.classList.add('valid')
+            confirmPassword.classList.remove('invalid')
+            confirmPassword.classList.add('valid')
+            message.textContent = ""
+        } else {
+            password.classList.remove('valid')
+            password.classList.add('invalid')
+            confirmPassword.classList.remove('valid')
+            confirmPassword.classList.add('invalid')
+            message.textContent = '*Passwords do not match'
+        }
     } else {
-        password.classList.remove('valid')
-        password.classList.add('invalid')
-        confirmPassword.classList.remove('valid')
-        confirmPassword.classList.add('invalid')
-        message.textContent = '*Passwords do not match'
-    }
-    } else {
-        password.classList.remove('invalid')
-        confirmPassword.classList.remove('invalid')
-        password.classList.remove('valid')
-        confirmPassword.classList.remove('valid')
-        message.textContent = ""
-    }
+            password.classList.remove('invalid')
+            confirmPassword.classList.remove('invalid')
+            password.classList.remove('valid')
+            confirmPassword.classList.remove('valid')
+            message.textContent = ""
+    } 
 }
 
 password.onchange = checkPass
